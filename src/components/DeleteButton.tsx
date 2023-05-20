72784cfa4fee1894fbe4e6568f5552e937bed575
richardenrico <richardenrico02@gmail.com>
2023-05-20 12:14:38 +0800

    feat: add DeleteButton component

---
+++ src/components/DeleteButton.tsx
@@ -0,0 +1,9 @@
+function DeleteButton() {
+    return (
+        <button className="p-2 text-lg bg-[#ff4500] text-white border-0 rounded cursor-pointer aspect-square">
+            X
+        </button>
+    );
+}
+
+export default DeleteButton;
