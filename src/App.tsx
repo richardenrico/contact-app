87f81e161993d6b0c785684390d826c37bf0293c
richardenrico <richardenrico02@gmail.com>
2023-06-14 17:02:45 +0800

    feat: add Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddPage from "./pages/AddPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="max-w-3xl my-0 mx-auto p-4">
       <h1 className="text-5xl mb-8">Aplikasi Kontak</h1>
+      <Navigation />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
