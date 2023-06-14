fca25810609928cba24ea70f260437ab8269196f
richardenrico <richardenrico02@gmail.com>
2023-06-14 17:46:51 +0800

    fix: fix tampilan

--- src/components/ContactItem.tsx
+++ src/components/ContactItem.tsx
@@ -13,8 +13,8 @@ interface IContactItemProps {
 
 function ContactItem({ id, imageUrl, name, onDelete, tag }: IContactItemProps) {
     return (
-        <div className="flex items-center my-6 mx-0 border-[1px] border-dashed border-black p-4 rounded-lg">
-            <Link to={`/${id}`}>
+        <div className="flex items-center my-6 justify-between border-[1px] border-dashed border-black p-4 rounded-lg">
+            <Link to={`/${id}`} className='flex'>
                 <ContactItemImage imageUrl={imageUrl} />
                 <ContactItemBody name={name} tag={tag} />
             </Link>
